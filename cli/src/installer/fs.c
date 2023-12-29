687935f06c628daebe534196f3dd808310a10db4
NikkieDev <dev.nikkie@gmail.com>
2023-12-29 02:00:17 +0100

    I broke it.. fuck sake

--- cli/src/installer/fs.c
+++ cli/src/installer/fs.c
@@ -1,7 +1,7 @@
 #include "headers/fs.h"
 #include "headers/cmdout.h"
-#include "headers/nw.h"
-#include "../../../libs/curl/curl.h"
+// #include "headers/nw.h"
+#include "../../libs/curl/curl.h"
 
 #include <stdio.h>
 
@@ -63,6 +63,41 @@ void create_win_dump()
   #endif
 }
 
+int get_serv_status()
+{
+  char buf[64];
+
+  CURL *hCurl;
+  
+  CURLcode curl_response;
+  curl_easy_setopt(hCurl, CURLOPT_URL, "http://node.kubyx.nl/TED/status"); /* TODO: create /TED/status endpoint with Laravel */
+  curl_easy_setopt(hCurl, CURLOPT_NOBODY, (long)1);
+
+  curl_response = curl_easy_perform(hCurl);
+  int http_code;
+  curl_easy_getinfo(hCurl, CURLINFO_RESPONSE_CODE, &http_code);
+
+  if (curl_response != CURLE_OK) {
+    int status = 0;
+    if (http_code != 0) {
+      snprintf(buf, sizeof(buf), "Couldn't reach file server! HTTP: %d", http_code);
+      status = 1;
+    }
+    else {
+      snprintf(buf, sizeof(buf), "Couldn't reach file server! Server not available.");
+      status = 2;
+    }
+
+    print_installer("network", buf, status);
+    return -1;
+  } else {
+    snprintf(buf, sizeof(buf), "Server reached with code: %d\n", http_code);
+    print_installer("network", buf, 0);
+
+    return 0;
+  }
+}
+
 void fetch_win_executable()
 {
   int reach_tries = 0;
@@ -82,7 +117,6 @@ try_connect:
 
     if (!fileserv_status) {
       print_installer("downloader", "Downloading executable files", 0);
-      download_win();
     } else {
       ++reach_tries;
       print_installer("network", "Trying again.", 2);
--- cli/src/installer/headers/nw.h
+++
@@ -1,7 +0,0 @@
-#ifndef NW_H
-#define NW_H
-
-int get_serv_status()
-void download_win();
-
-#endif
